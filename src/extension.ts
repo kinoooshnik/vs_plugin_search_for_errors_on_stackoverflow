d7d82e0cf07c15be3a35eb74ac1881187486435b
Alexander Semiletov <sasha.semiletow2011@gmail.com>
2019-11-20 11:08:34 +0300

    Refactoring code

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,11 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "search-for-errors-on-stackoverflow" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.searchForErrorOnStackoverflow', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('extension.searchForErrorOnStackoverflow', () => {
+		// getting a list of problems
 		let diagnostics: [vscode.Uri, vscode.Diagnostic[]][] = vscode.languages.getDiagnostics();
 
+		// formation of the error selection list
 		let errorMessages: string[] = [];
 		diagnostics.forEach((value) => {
 			value[1].forEach((value) => {
@@ -27,17 +17,17 @@ export function activate(context: vscode.ExtensionContext) {
 			})
 		});
 
+		// show the selection window
 		vscode.window.showQuickPick(errorMessages)
+			// process the response
 			.then((value) => {
 				if (value !== undefined)
 					openBrowser(value);
 			});
-	});
-
-	context.subscriptions.push(disposable);
+		})
+	);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
 
 function openBrowser(str: string) {
