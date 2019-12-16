3d9a34fa82280e1248138eaad7175407817f8b8f
Evgeniy Vasilyev <ju5tc4t@gmail.com>
2019-12-16 18:45:36 +0300

    Make code even better

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,31 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('extension.searchForErrorOnStackoverflow', () => {
-		// getting a list of problems
-		let diagnostics: [vscode.Uri, vscode.Diagnostic[]][] = vscode.languages.getDiagnostics();
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension.searchForErrorOnStackoverflow', () => {
+			// getting a list of problems
+			let diagnostics: [vscode.Uri, vscode.Diagnostic[]][] = vscode.languages.getDiagnostics();
 
-		// formation of the error selection list
-		let errorMessages: string[] = [];
-		diagnostics.forEach((value) => {
-			value[1].forEach((value) => {
-				let error: string = value.message;
-				if (value.source !== undefined) {
-					error += ' ' + value.source;
-				}
-				errorMessages.push(error);
-			})
-		});
-
-		// show the selection window
-		vscode.window.showQuickPick(errorMessages)
-			// process the response
-			.then((value) => {
-				if (value !== undefined)
-					openBrowser(value);
+			// formation of the error selection list
+			let errorMessages: string[] = [];
+			diagnostics.forEach((value) => {
+				value[1].forEach((value) => {
+					let error: string = value.message;
+					if (value.source !== undefined) {
+						error += ' ' + value.source;
+					}
+					errorMessages.push(error);
+				})
 			});
-	})
+
+			// show the selection window
+			vscode.window.showQuickPick(errorMessages)
+				// process the response
+				.then((value) => {
+					if (value !== undefined)
+						openBrowser(value);
+				});
+		})
 	);
 }
 
