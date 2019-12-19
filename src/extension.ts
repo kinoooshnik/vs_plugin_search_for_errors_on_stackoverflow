aff72bd0a3703d213a9e7e7254f1a4e267bcc8ec
Evgeniy Vasilyev <ju5tc4t@gmail.com>
2019-12-19 05:49:28 +0300

    Added WebView with questions and answers

--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,41 @@
 import * as vscode from 'vscode';
 import * as models from './models';
+import * as view from './view';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('extension.searchForErrorOnStackoverflow', () => {
+			models.SearchPage.search('typescript').then(view.display);
 			// getting a list of problems
-			let diagnostics: [vscode.Uri, vscode.Diagnostic[]][] = vscode.languages.getDiagnostics();
+			// let diagnostics: [vscode.Uri, vscode.Diagnostic[]][] = vscode.languages.getDiagnostics();
 
-			// formation of the error selection list
-			let errorMessages: string[] = [];
-			diagnostics.forEach((value) => {
-				value[1].forEach((value) => {
-					let error: string = value.message;
-					if (value.source !== undefined) {
-						error += ' ' + value.source;
-					}
-					errorMessages.push(error);
-				})
-			});
+			// // formation of the error selection list
+			// let errorMessages: string[] = [];
+			// diagnostics.forEach((value) => {
+			// 	value[1].forEach((value) => {
+			// 		let error: string = value.message;
+			// 		if (value.source !== undefined) {
+			// 			error += ' ' + value.source;
+			// 		}
+			// 		errorMessages.push(error);
+			// 	})
+			// });
 
-			// show the selection window
-			vscode.window.showQuickPick(errorMessages)
-				// process the response
-				.then((value) => {
-					if (value !== undefined)
-						// как использовать
-						// функция асинхронная, поэтому обрабатывать ее ответ надо через callback (.then(функция, в которую передается результат))
-						// возвращает объект SearchPage
-						// в будущем немного порежу данные, которые есть изначально, потому что запрос на все данные выполняется долго
-						// для получения более полных данных нужно будет вызвать соответствующий метод класса вопроса
-						models.SearchPage.search('typescript').then(console.log);
-						openBrowser(value!);
-				});
+			// // show the selection window
+			// vscode.window.showQuickPick(errorMessages)
+			// 	// process the response
+			// 	.then((errorMessage) => {
+			// 		if (errorMessage)
+			// 			// как использовать
+			// 			// функция асинхронная, поэтому обрабатывать ее ответ надо через callback (.then(функция, в которую передается результат))
+			// 			// возвращает объект SearchPage
+			// 			// в будущем немного порежу данные, которые есть изначально, потому что запрос на все данные выполняется долго
+			// 			// для получения более полных данных нужно будет вызвать соответствующий метод класса вопроса
+			// 			models.SearchPage.search(errorMessage).then(view.display);
+			// 	});
 		})
 	);
 }
-
 
 export function deactivate() { }
 
---
+++ src/view.ts
@@ -0,0 +1,43 @@
+import { SearchPage } from './models';
+import * as vscode from 'vscode';
+
+export function display(page: SearchPage): void {
+    const panel = vscode.window.createWebviewPanel(
+        'stackoverlfowView',
+        'Stack Overflow',
+        vscode.ViewColumn.Beside
+    );
+
+    panel.webview.html = constructWebView(page);
+}
+
+function constructWebView(page: SearchPage): string {
+    let body: string;
+
+    if (page.totalResults === 0) {
+        body = `<h1>No questions found for query '${page.query}'</h1>`;
+    } else {
+        body = `<h1>Questions for query '${page.query}':</h1>`;
+        page.results.forEach(element => {
+            body += `<h1>${element.title}</h1>`;
+            body += element.body;
+            body += `<h1>Answer:</h1>`
+            body += element.answers[0].body;
+            body += `<hr>`;
+        });
+    }
+
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Security-Policy" content="default-src 'none';">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+        <title>Stack Overflow</title>
+    </head>
+    <body>
+        ${body}
+    </body>
+    </html>`;
+}
